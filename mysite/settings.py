819c04a1f84722891f4a4418deece975737e7052
anshu0157 <anshu0157@gmail.com>
2021-02-06 16:46:47 +0530

    'secondcommit'

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+stat=os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL='/mysite/img/'
+STATICFILES_DIRS=[stat,]
